3696b9c787c4c7a7c12bac9a7ccbe1c461c2ff42
Devwardhan Kothari <88496527+Devwardhan-Kothari@users.noreply.github.com>
2024-10-14 23:15:09 +0530

    Add functionality and interativity

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let first_operand = 0;
+let first_operand = null;
 let second_operand = null;
-let operator = null;
+let operation = null;
+let displayValue = "0";
 
 function add(a, b) {
     return Number(a) + Number(b);
@@ -16,9 +17,8 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if(Number(b) === 0) {
-        // something witty
-        console.log("Just a sny log for now.");
-        return;
+        display.textContent = "Bruh";
+        return 'Bruh';
     }
     return Number(a) / Number(b);
 }
@@ -37,3 +37,57 @@ function operator(a, b, op) {
             console.log("This should not even be possible!");
     }
 }
+
+const container = document.querySelector(".container");
+container.addEventListener('click', (event) => {
+    const display = document.querySelector("#display");
+    let target = event.target;
+    if(target.classList.length > 0) {
+        if(target.className === "number") {
+            if(displayValue.length === 10) return;
+            if(displayValue === "0" || (operation !== null && displayValue === "")) {
+                displayValue = target.textContent;
+                display.textContent = displayValue;
+            } else {
+                displayValue += target.textContent;
+                display.textContent = displayValue;
+            }
+        } else {
+            first_operand = displayValue;
+            operation = target.textContent;
+            displayValue = "";
+        }
+    } else {
+        switch(target.id) {
+            case 'decimal':
+                if(displayValue.includes(".") || displayValue.length === 10) return;
+                else {
+                    displayValue += '.';
+                    display.textContent = displayValue;
+                }
+                break;
+            case 'AC':
+                first_operand = null;
+                second_operand = null;
+                operation = null;
+                displayValue = "0";
+                display.textContent = displayValue;
+                break;
+            case 'equals':
+                if(operation === null) return;
+                second_operand = displayValue;
+                displayValue = operator(first_operand, second_operand, operation).toString().substring(0, 10);
+                if(displayValue === 'Bruh') {
+                    first_operand = null;
+                    second_operand = null;
+                    operation = null;
+                    displayValue = "0";
+                } else {
+                    first_operand = displayValue;
+                    second_operand = null;
+                    operation = null;
+                    display.textContent = displayValue;
+                }
+        }
+    }
+});
